fee822914ad514ce1633ce8ac776eaccc36ed4e4
unknown <thejohnny26@gmail.com>
2020-12-09 17:22:12 +0200

    create folder Lesson 2 and add Task 1

---
+++ Lesson 2/Task 1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num[10];
+	int num2;
+	int counter = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "enter school numbers: ";
+			cin >> num[i];
+	}
+	cout << "enter ur school:";
+	cin >> num2;
+	for (int i = 0; i < 10; i++)
+	{
+		if (num[i] == num2)
+		{
+			counter++;
+		}	
+	}
+	if (counter >= 1)
+	{
+		cout << "I know that school: ";
+	}
+	else {
+		cout << "I dont know that school: ";
+	}
+}
