036d896213b2af74b2695ba3c3d4a9681224066b
unknown <thejohnny26@gmail.com>
2020-12-30 23:29:58 +0200

    change style code

--- Lesson 2/Task 3.cpp
+++ Lesson 2/Task 3.cpp
@@ -4,36 +4,36 @@ using namespace std;
 
 int main()
 {
-    int maxValue = 0;
-	int arraysize = 1;
-	int numbers[arraysize];
-	int zero = 1;
-	int i = 0;
+  int maxValue = 0;
+  int arraysize = 1;
+  int numbers[arraysize];
+  int zero = 1;
+  int i = 0;
 
-		while (zero != 0)
-		{
-			cout << "Enter number: ";
-			cin >> zero;
-			numbers[i] = zero;
-			arraysize++;
-			i++;
-		}
-		for(int i = 0; i < arraysize;i++)
-		{
-		    if(numbers[i] > maxValue)
-            maxValue = numbers[i];
-		}
+ while (zero != 0)
+ {
+  cout << "Enter number: ";
+  cin >> zero;
+  numbers[i] = zero;
+  arraysize++;
+  i++;
+ }
+ for (int i = 0; i < arraysize; i++)
+ {
+  if (numbers[i] > maxValue)
+  maxValue = numbers[i];
+ }
 
-		for (int i = 0; i < arraysize - 1; i++)
-		{
-			for(int j = 0; j < maxValue - numbers[i] / 2 ;j++)
-                {
-                    cout << " ";
-                }
-            for(int k = 0; k < numbers[i];k++)
-                {
-                    cout << "*";
-                }
-                cout << endl;
-		}
+for (int i = 0; i < arraysize - 1; i++)
+{
+ for (int j = 0; j < maxValue - numbers[i] / 2; j++)
+  {
+   cout << " ";
+  }
+  for (int k = 0; k < numbers[i]; k++)
+  {
+  cout << "*";
+  }
+  cout << endl;
+  }
 }
