383990200b968897a38869214e92e8e98168072a
unknown <thejohnny26@gmail.com>
2021-01-18 17:59:13 +0200

    change

--- Lesson 3/Task 2.cpp
+++ Lesson 3/Task 2.cpp
@@ -4,43 +4,43 @@ using namespace std;
 
 int main()
 {
-    char mysting[100];
-    int b = 0,
-    int word;
-    int c = 0;
-    int i = 0;
-    cout << "Enter your line: ";
-    cin.getline(mysting, 100);
+  char mysting[100];
+  int lenght = 0;
+  int word;
+  int maxlenght = 0;
+  int index = 0;
+  cout << "Enter your line: ";
+  cin.getline(mysting, 100);
 
-    while (mysting[i] != 0)
+  while (mysting[index] != 0)
+  {
+    if (mysting[index] >= 'a' && mysting[index] <= 'z' || mysting[index] >= 'A' && mysting[index] <= 'Z')
     {
-        if (mysting[i] >= 'a' && mysting[i] <= 'z' || mysting[i] >= 'A' && mysting[i] <= 'Z')
-        {
-            b++;
-        }
-        else
-        {
-            if (b > c)
-            {
-                c = b;
-                word = i - c;
-            }
-            b = 0;
-        }
-        if (mysting[i + 1] == 0)
-        {
-            if (b > c)
-            {
-                c = b;
-                word = i - c + 1;
-            }
-        }
-        i++;
+      lenght++;
     }
-    cout << "The largest word is: ";
-    for (int i = 0; i < c; i++)
+    else
     {
-        cout << mysting[word];
-        word++;
+      if (lenght > maxlenght)
+      {
+        maxlenght = lenght;
+        word = index - maxlenght;
+      }
+      lenght = 0;
     }
+    if (mysting[index + 1] == 0)
+    {
+      if (lenght > maxlenght)
+      {
+        maxlenght = lenght;
+        word = index - maxlenght + 1;
+      }
+    }
+    index++;
+  }
+  cout << "The largest word is: ";
+  for (int i = 0; i < maxlenght; i++)
+  {
+    cout << mysting[word];
+    word++;
+  }
 }
